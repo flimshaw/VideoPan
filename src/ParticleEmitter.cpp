e9276c87481c3f46d07bf79cf9d4883b0f4c8969
Charlie Hoey <charlie.hoey@gmail.com>
2013-04-02 22:10:18 -0400

    instantiated particle emitter without crashing!

--- src/ParticleEmitter.cpp
+++ src/ParticleEmitter.cpp
@@ -1,11 +1,17 @@
 // This is a class that emits particles.
 // What should it look like?
 #include "ParticleEmitter.h";
+#include "stdio.h";
+#include "cinder/app/AppBasic.h"
+
+using namespace std;
+using namespace ci;
+using namespace ci::app;
 
 ParticleEmitter::ParticleEmitter() {
 
 }
 
 ParticleEmitter::ParticleEmitter(int max_particles) {
-
+	console() << "TESTING" << std::endl;
 }
