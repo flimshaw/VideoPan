bcca3f1a6d5b54ff6d24338921151f9c411eec61
Charlie <charlie.hoey@gmail.com>
2013-04-13 14:12:35 -0400

    added template file

--- scripts/stitchFrame.js
+++ scripts/stitchFrame.js
@@ -2,14 +2,13 @@
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 var mustache = require('mustache');
+var program = require('commander');
 
-// get the frame number
-var frameNumber = process.argv[2];
+program
+  .version('0.1.0')
+  .usage('[options] [srcDir]')
+  .option('-n, --frameNumber <frameNumber>', 'specify the log format string')
+  .parse(process.argv);
 
-// src directory
-var srcDir = process.argv[3]
+var srcDir = program.args[0] || process.exit(0)
 
-// create a folder for our renderings
-mkdirp.sync(srcDir + "/render");
-
-// generate a pto file 
