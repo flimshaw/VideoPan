c5cc78b86ca7e5984b895c07608ad3354638ea28
Charlie <charlie.hoey@gmail.com>
2013-04-12 21:05:00 -0400

    commiting before changing to a standard node module structure

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var path = require('path')
 var mkdirp = require('mkdirp')
+var ffmpeg = require('ffmpeg');
 
 // the video file is the first argument
 var videoFile = process.argv[2];
@@ -8,6 +9,7 @@ var videoFile = process.argv[2];
 // create a directory for the videos in a folder named the file name, ie REAR/
 var videoDir = path.basename(videoFile, path.extname(videoFile));
 
+// create our video directory, or exit with an error
 mkdirp(videoDir, function(e) {
 	if(e) {
 		switch(e) {
@@ -23,3 +25,24 @@ mkdirp(videoDir, function(e) {
 		console.log("Directory verified.");
 	}
 });
+
+// TODO: Verify the video file is a video file
+
+try {
+    new ffmpeg(videoFile, function (err, video) {
+        if (!err) {
+            processVideoFile(video);
+        } else {
+            console.log('Error: ' + err);
+        }
+    });
+} catch (e) {
+    console.log(e.code);
+    console.log(e.msg);
+}
+
+function processVideoFile(video) {
+	console.log(video);
+}
+
+ffmpeg(videoFile);
