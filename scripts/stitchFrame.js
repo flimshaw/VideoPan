63c2c5adc10f307417cbf0f2522f5286a2c3f1f6
Charlie <charlie.hoey@gmail.com>
2013-04-13 15:59:09 -0400

    working on making the script loop through all frame numbers

--- scripts/stitchFrame.js
+++ scripts/stitchFrame.js
@@ -4,6 +4,10 @@ var mkdirp = require('mkdirp');
 var mustache = require('mustache');
 var path = require('path');
 var program = require('commander');
+var querystring = require('querystring');
+var sprintf = require('sprintf').sprintf;
+var execSync = require('execSync');
+var _ = require('underscore');
 
 program
   .version('0.1.0')
@@ -18,34 +22,55 @@ var templateString = fs.readFileSync(program.template, 'utf8');
 
 var Stitcher = function(templateString, frameNumber, srcDir) {
 	
+	var self = this;
+
 	this.templateString = templateString;
 	this.frameNumber = frameNumber;
 	this.srcDir = srcDir;
-	this.destDir = path.join(srcDir, 'render');
+	this.destDir = path.join(srcDir, 'render', this.frameNumber.toString());
 
 	// create destination directory
 	mkdirp.sync(this.destDir);
 
 	// generate a pto file for this directory
-	this.generatePtoFile();
+	this.ptoFilePath = this.generatePtoFile();
 
-	console.log(this.getSrcPath());
+	// run nona
+	console.log("Remapping...");
+	var nonaCmd = sprintf("nona -z NONE -r ldr -m TIFF_m -o %s/ -v %s", this.destDir, this.ptoFilePath);
+	var nona = execSync.code(nonaCmd);
 
-	console.log("stitcher started");
-	console.log(this.ptoFile);
+	// run enblend
+	console.log("Enblending...");
+	var enblendCmd = sprintf("enblend -l 9 -v -w -o %s", path.join(this.destDir, this.frameNumber + "_rendered.jpg"));
+	_.each([0, 1, 2], function(file) {
+		enblendCmd += sprintf(" %s/%04d.tif", self.destDir, file);
+	});
+	var enblend = execSync.code(enblendCmd);
+
 }
 
-Stitcher.prototype.getSrcFilePath = function(camera) {
+Stitcher.prototype.renderFrame = function(frameNumber) {
+	this.generatePtoFile(frameNumber);
+
+}
+
+Stitcher.prototype.getDestDir = function(frameNumber) {
+	return path.join(this.srcDir, 'render', frameNumber.toString());
+}
+
+Stitcher.prototype.getSrcFilePath = function() {
 	var group = Math.floor((this.frameNumber - 1) / 1000);
-	var srcFilePath = path.join(group.toString(), this.frameNumber + '.jpg');
+	var srcFilePath = path.join(group.toString(), sprintf("%05d.jpg", Number(this.frameNumber)));
 	return srcFilePath;
 }
 
-Stitcher.prototype.generatePtoFile = function() {
+Stitcher.prototype.generatePtoFile = function(frameNumber) {
 	var view = {
 		frameNumber: this.frameNumber,
 		srcDir: this.srcDir,
-		destDir: path.join(this.destDir, this.frameNumber.toString()),
+		srcFilePath: this.getSrcFilePath(),
+		destDir: this.destDir,
 		destFile: this.frameNumber + ".pto"
 	}
 
@@ -53,7 +78,9 @@ Stitcher.prototype.generatePtoFile = function() {
 	mkdirp.sync(view.destDir);
 
 	var ptoFile = mustache.render(this.templateString, view);
-	fs.writeFileSync(path.join(view.destDir, view.destFile), ptoFile);
+	var ptoFilePath = path.join(view.destDir, view.destFile);
+	fs.writeFileSync(ptoFilePath, ptoFile);
+	return ptoFilePath;
 }
 
 var stitcher = new Stitcher(templateString, program.frameNumber, srcDir);
