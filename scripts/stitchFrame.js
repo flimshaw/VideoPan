85daf0e78528b195083d894597229c23ea63c3ae
Charlie <charlie.hoey@gmail.com>
2013-04-13 17:12:37 -0400

    lots of updates, currently stitching properly!

--- scripts/stitchFrame.js
+++ scripts/stitchFrame.js
@@ -8,6 +8,7 @@ var querystring = require('querystring');
 var sprintf = require('sprintf').sprintf;
 var execSync = require('execSync');
 var _ = require('underscore');
+var sleep = require('sleep');
 
 program
   .version('0.1.0')
@@ -20,6 +21,7 @@ program
 var srcDir = program.args[0] || process.exit(0)
 
 var templateString = fs.readFileSync(program.template, 'utf8');
+
 
 var Stitcher = function(templateString, startFrame, count, srcDir) {
 	
@@ -51,13 +53,13 @@ Stitcher.prototype.renderFrame = function(frameNumber) {
 	var ptoFilePath = this.generatePtoFile(frameNumber);
 
 	// run nona
-	console.log("Remapping...");
+	console.log(sprintf("Remapping frame %05d ...", frameNumber));
 	var nonaCmd = sprintf("nona -z NONE -r ldr -m TIFF_m -o %s/ -v %s", this.getDestDir(frameNumber), ptoFilePath);
 	var nona = execSync.code(nonaCmd);
 	
 	// run enblend
-	console.log("Enblending...");
-	var enblendCmd = sprintf("enblend -l 9 -v -w -o %s", path.join(this.getDestDir(frameNumber), frameNumber + "_rendered.jpg"));
+	console.log(sprintf("Enblending frame %05d ...", frameNumber));
+	var enblendCmd = sprintf("enblend -l 9 -v -w -o %s", this.getDestFilePath(frameNumber));
 	_.each([0, 1, 2], function(file) {
 		enblendCmd += sprintf(" %s/%04d.tif", self.getDestDir(frameNumber), file);
 	});
@@ -68,10 +70,42 @@ Stitcher.prototype.renderFrame = function(frameNumber) {
 		fs.unlink(sprintf("%s/%04d.tif", self.getDestDir(frameNumber), file));
 	});
 
+	// remove pto file
+	fs.unlink(ptoFilePath);
+
+	// move the final file to the appropriate directory
+	this.submitFile(frameNumber);
+
+	this.cleanup();
+
+}
+
+Stitcher.prototype.cleanup = function() {
+	execSync.code("rmdir " + this.destDir + "/temp*");
+}
+
+// submit our file
+Stitcher.prototype.submitFile = function(frameNumber) {
+	var self = this;
+	mkdirp.sync(this.getFinalFileDir());
+	fs.renameSync(this.getDestFilePath(frameNumber), this.getFinalFilePath(frameNumber));
 }
 
 Stitcher.prototype.getDestDir = function(frameNumber) {
-	return path.join(this.srcDir, 'render', frameNumber.toString());
+	return path.join(this.srcDir, 'render', 'temp_' + frameNumber.toString());
+}
+
+Stitcher.prototype.getDestFilePath = function(frameNumber) {
+	return path.join(this.getDestDir(frameNumber), frameNumber + "_rendered.jpg");
+}
+
+Stitcher.prototype.getFinalFilePath = function(frameNumber) {
+	return path.join(this.getFinalFileDir(frameNumber), sprintf("%05d.jpg", frameNumber));
+}
+
+Stitcher.prototype.getFinalFileDir = function(frameNumber) {
+	var destGroup = Math.floor((frameNumber - 1) / 1000) || 0;
+	return path.join(this.destDir, destGroup.toString());
 }
 
 Stitcher.prototype.getSrcFilePath = function(frameNumber) {
